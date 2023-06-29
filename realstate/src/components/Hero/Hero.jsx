c3672c684781f7164792a7bdc2b894e5b3f9dc64
paudel54 <2sansrit@gmail.com>
2023-06-29 10:59:10 +0545

    added Hero Sections

--- realstate/src/components/Hero/Hero.jsx
+++ realstate/src/components/Hero/Hero.jsx
@@ -1,7 +1,66 @@
 import React from "react";
-
+import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 const Hero = () => {
-  return <div>This is Hero Section</div>;
+  return (
+    <section className="hero-Wrapper">
+      <div className="paddings innerWidth flexCenter hero-container">
+        {/* left side  */}
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover <br />
+              Most Suitable <br /> Property
+            </h1>
+          </div>
+          <div className="flexColStart hero-des">
+            <span className="secondaryText">
+              Find a variety of properties that suit you very easily.
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
+          </div>
+          <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={700} end={1200} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1800} end={2150} duration={2} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={5} end={21} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award Winnings</span>
+            </div>
+          </div>
+        </div>
+        {/* Right Side */}
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="hero img" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
