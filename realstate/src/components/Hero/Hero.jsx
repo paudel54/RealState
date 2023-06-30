31acddeb14f94665e424eb9a51d4d9f507384367
paudel54 <2sansrit@gmail.com>
2023-06-30 15:50:42 +0545

    Project landing Page completed

--- realstate/src/components/Hero/Hero.jsx
+++ realstate/src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="hero-Wrapper">
@@ -10,10 +11,17 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}
+            >
               Discover <br />
               Most Suitable <br /> Property
-            </h1>
+            </motion.h1>
           </div>
           <div className="flexColStart hero-des">
             <span className="secondaryText">
@@ -23,7 +31,7 @@ const Hero = () => {
               Forget all difficulties in finding a residence for you
             </span>
           </div>
-          <div className="flexCenter search-bar">
+          <div className="flexCenter search-bar ">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
@@ -54,9 +62,17 @@ const Hero = () => {
         </div>
         {/* Right Side */}
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div
+            className="image-container"
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "spring",
+            }}
+          >
             <img src="./hero-image.png" alt="hero img" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
